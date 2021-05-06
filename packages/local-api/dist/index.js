d5f006fc330dbc43843d8cbf321d5ebcd640e671
micmob <micfoee@gmail.com>
2021-05-06 15:48:56 +0300

    + build

--- packages/local-api/dist/index.js
+++ packages/local-api/dist/index.js
@@ -7,6 +7,7 @@ exports.serve = void 0;
 var express_1 = __importDefault(require("express"));
 var serve = function (port, filename, dir) {
     var app = express_1.default();
+    app.use(express_1.default.static('../../local-client/build'));
     return new Promise(function (resolve, reject) {
         app.listen(port, resolve).on('error', reject);
     });
--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -1,7 +1,10 @@
 import express from 'express';
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
 export const serve = (port: number, filename: string, dir: string) => {
     const app = express();
+
+    app.use(express.static('../../local-client/build'));
 
     return new Promise<void>((resolve, reject) => {
         app.listen(port, resolve).on('error', reject);
